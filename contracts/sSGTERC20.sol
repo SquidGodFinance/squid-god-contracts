3b0872572ad9ceb40a6d611ff9d928638beb8945
SquidGodFinance <SquidGodFinance@outlook.com>
2021-10-30 14:09:19 +0800

    change token name

--- contracts/sSGTERC20.sol
+++ contracts/sSGTERC20.sol
@@ -1047,7 +1047,7 @@ contract sSGT is ERC20Permit, Ownable {
 
     mapping ( address => mapping ( address => uint256 ) ) private _allowedValue;
 
-    constructor() ERC20("Staked SGT", "sSGT", 9) ERC20Permit() {
+    constructor() ERC20("GAME Token", "GAME", 9) ERC20Permit() {
         initializer = msg.sender;
         _totalSupply = INITIAL_FRAGMENTS_SUPPLY;
         _gonsPerFragment = TOTAL_GONS.div(_totalSupply);
